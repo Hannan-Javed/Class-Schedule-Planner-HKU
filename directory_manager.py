65fbe8f779977e28c217318f2a38a45ca39dc2bd
Hannan-Javed <hannansahib@gmail.com>
2025-01-01 21:27:46 +0500

    ask for directory once

--- directory_manager.py
+++ directory_manager.py
@@ -3,18 +3,19 @@ from tkinter import filedialog
 from utils import list_menu_selector  # Added import
 
 class DirectoryManager:
+    def __init__(self):
+        self.directory = None
     def get_default_download_directory(self):
         home_directory = os.path.expanduser("~")
         return os.path.join(home_directory, 'Downloads') if os.name in ['posix', 'nt'] else None
 
     def make_directory(self, term, course):
-        choice = list_menu_selector('directory', 'Please select the directory to save the files:', ['Downloads', 'Input Directory Path'])
-        directory = self.get_default_download_directory() if choice == 'Downloads' else self.prompt_directory()
-        if directory:
-            directory = os.path.join(directory, term, course)
-            os.makedirs(directory, exist_ok=True)
-            return directory
-        return None
+        if not self.directory:
+            choice = list_menu_selector('directory', 'Please select the directory to save the files:', ['Downloads', 'Input Directory Path'])
+            self.directory = self.get_default_download_directory() if choice == 'Downloads' else self.prompt_directory()
+        dir = os.path.join(self.directory, term, course)
+        os.makedirs(dir, exist_ok=True)
+        
 
     def prompt_directory(self):
         while True:
